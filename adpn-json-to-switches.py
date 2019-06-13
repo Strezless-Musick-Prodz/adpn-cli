d30f906864da36037f1a8df6ea3e825fc77d133a
C. Johnson <charlesw.johnson@archives.alabama.gov>
2019-06-13 16:30:58 -0500

    JSON input: if you paste in a packet from the local test on the remote side, it should make us eof all filled in parameter values, signal remote=1, pass in Plugin ID, etc.

--- adpn-json-to-switches.py
+++ adpn-json-to-switches.py
@@ -8,11 +8,44 @@ if __name__ == '__main__' :
 	script = sys.argv[0]
 	script = os.path.basename(script)
 
-	jsonInput = "".join(fileinput.input())
+	exitcode = 0
 	
-	table = json.loads(jsonInput)
+	try :
+		jsonInput = "".join(fileinput.input())
 	
-	if ('Ingest Title' in table) :
-		print('--au_title=%(name)s' % {"name": table['Ingest Title']})
-	if ('Plugin JAR' in table) :
-		print('--jar=%(url)s' % {"url": table['Plugin JAR']})
+		ref=re.match("^([A-Za-z0-9]+\s*)+:\s*([{].*[}])\s*$", jsonInput)
+		if (ref) :
+			jsonInput = ref[2]
+	
+		table = json.loads(jsonInput)
+	
+		if ('Ingest Title' in table) :
+			print('--au_title=%(name)s' % {"name": table['Ingest Title']})
+
+		if ('Plugin JAR' in table) :
+			print('--jar=%(url)s' % {"url": table['Plugin JAR']})
+		if ('Plugin ID' in table) :
+			print('--plugin-id=%(id)s' % {"id": table['Plugin ID']})
+		elif ('Plugin Name' in table) :
+			print('--plugin=%(name)s' % {"name": table['Plugin Name']})
+
+		if ('File Size ' in table) :
+			print('--remote=1')
+				
+		if ('parameters' in table) :
+			if len(table) > 0 :
+				for param, value in table['parameters'] :
+					print('--%(KEY)s=%(VALUE)s' % {"KEY": param, "VALUE": value})
+					
+	except json.decoder.JSONDecodeError as e :
+	
+		print(
+			("[%(script)s] JSON encoding error. Could not extract key-value pairs from "
+			+ "the provided data:\n\n%(json)s")
+			% {"script": script, "json": jsonInput},
+			file=sys.stderr
+		)
+		exitcode = 1
+		
+	sys.exit(exitcode)
+		
