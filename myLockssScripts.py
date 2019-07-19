dddcc2b1b18b7dd5965e49dc87af4015c777447a
C. Johnson <charlesw.johnson@archives.alabama.gov>
2019-07-19 16:25:46 -0500

    Doc string.

--- myLockssScripts.py
+++ myLockssScripts.py
@@ -13,6 +13,8 @@ import subprocess
 from subprocess import PIPE
 
 class myPyPipeline :
+	"""Given a sequence of shell processes, pipe output from one to input for the next, using POSIX pipes.
+	"""
 	def __init__ (self, pipeline) :
 		self.pipeline = pipeline
 	
@@ -41,7 +43,6 @@ class myPyPipeline :
 		_lastproc.stdout.close()
 		
 		return (buf, errbuf, [proc.returncode for proc in processes])
-
 	
 class myPyCommandLine :
 	"""Parse a Unix-style shell command-line, separating out configuration parameters and files/objects.
