16af2371614eadecb52fab81a8ed6f410ee1929a
C. Johnson <charlesw.johnson@archives.alabama.gov>
2021-06-29 10:23:44 -0500

    adpn-stage-content.py, adpn-make-manifest.py / Stage content can go to an SFTP server using pysftp module; adpn-make-manifest now typically saves to a file rather than dumping to stdout.

---
+++ ADPNPreservationPackage.py
@@ -0,0 +1,88 @@
+import io, os, sys
+import fileinput, stat
+import subprocess
+import re, json
+import urllib, ftplib, pysftp
+import math
+from datetime import datetime
+from io import BytesIO
+from ftplib import FTP
+from getpass import getpass
+from myLockssScripts import myPyCommandLine, myPyJSON
+
+class myLockssPlugin :
+    
+    def __init__ (self, jar) :
+        self._jar = jar
+    
+    @property
+    def jar (self) :
+        return self._jar
+        
+    def get_manifest_filename (self) :
+        return "manifest.html" #FIXME
+    
+class ADPNPreservationPackage :
+    
+    def __init__ (self, path, parameters, switches) :
+        self._path = path
+        self._parameters = parameters
+        self._switches = switches
+        self._plugin = myLockssPlugin(jar=self.switches["jar"])
+    
+    @property
+    def path (self) :
+        return self._path
+        
+    @property
+    def parameters (self) :
+        return self._parameters
+    
+    @property
+    def switches (self) -> dict :
+        return self._switches
+    
+    @property
+    def plugin (self) -> myLockssPlugin :
+        return self._plugin
+    
+    def get_path (self, item=None) -> str :
+        path = self.path
+        if item is not None :
+            path = os.path.join(path, item)
+        return path
+        
+    def has_manifest (self) -> bool :
+        has_it = False
+        try :
+            stat_results = os.stat(self.get_path(self.plugin.get_manifest_filename()))
+            has_it = stat.S_ISREG(stat_results.st_mode)
+        except FileNotFoundError as e :
+            pass
+        return has_it
+    
+    def make_manifest (self) :
+        try :
+            jsonParams = json.dumps(self.parameters)
+            cmdline = [
+                "adpn-make-manifest.py",
+                    "--jar="+self.switches['jar'],
+                    "--parameters="+jsonParams,
+                    "--local="+self.switches['local']
+            ]
+            if ( 'proxy' in self.switches.keys() ) and ( self.switches['proxy'] is not None ) :
+                cmdline.append("--proxy="+self.switches['proxy'])
+            if ( 'port' in self.switches.keys() ) and ( self.switches['port'] is not None ) :
+                cmdline.append("--port="+self.switches['port'])
+            if ( 'tunnel' in self.switches.keys() ) and ( self.switches['tunnel'] is not None ) :
+                cmdline.append("--tunnel="+self.switches['tunnel'])
+            if ( 'tunnel-port' in self.switches.keys() ) and ( self.switches['tunnel-port'] is not None ) :
+                cmdline.append( "--tunnel-port="+self.switches['tunnel-port'])
+            
+            buf = subprocess.check_output(cmdline, encoding="utf-8")
+        except subprocess.CalledProcessError as e :
+            code = e.returncode
+            buf = e.output
+        
+        return buf
+
--- adpn-make-manifest.py
+++ adpn-make-manifest.py
@@ -7,21 +7,52 @@
 
 import os
 import sys
+import stat
 import json
 import urllib.parse
 import myLockssScripts
 from getpass import getpass
 from myLockssScripts import myPyCommandLine, myPyPipeline
 
+def get_templatedirectory () :
+    scriptpath = os.path.realpath(__file__)
+    scriptname = os.path.basename(scriptpath)
+    scriptdir = os.path.dirname(scriptpath)
+    return scriptdir
+
+def get_filesystemlocation (file) :
+    location = None
+    try :
+        fstat = os.stat(file)
+        if stat.S_ISDIR(fstat.st_mode) :
+            location = file
+        else :
+            location = os.path.dirname(file)
+            
+    except FileNotFoundError as e :
+        pass
+    return location
+
+def read_makemanifest_html (parameters) :
+    infile = ( "%(path)s/%(file)s.sprintf" % {"path": get_templatedirectory(), "file": file} )
+    
+    instream = open(infile, "r")
+    template = "\n".join([ line.rstrip() for line in instream ])
+    instream.close()
+    
+    html = ( template % parameters )
+    return html
+
 if __name__ == '__main__' :
 
-	(argv, switches) = myPyCommandLine(sys.argv, defaults={}).parse()
+	(argv, switches) = myPyCommandLine(sys.argv, defaults={ "parameters": "{}", "local": None }).parse()
 
-	scriptname = os.path.basename(sys.argv[0])
-	scriptdir = os.path.dirname(sys.argv[0])
+	scriptpath = os.path.realpath(__file__)
+	scriptname = os.path.basename(scriptpath)
+	scriptdir = get_templatedirectory()
 	
 	os.environ["PATH"] = ":".join([scriptdir, os.environ["PATH"]])
-
+	
 	scriptLine = ['adpn-plugin-details.py', '--output=text/tab-separated-values']
 	
 	detailsScript = myPyPipeline([ scriptLine + sys.argv[1:] ])
@@ -29,17 +60,30 @@ if __name__ == '__main__' :
 	
 	settings = [ line.rstrip().split("\t") for line in filter(lambda text: len(text.rstrip())>0, buff.split("\n")) ]
 	settings = [ (line[1], line[2]) for line in settings if line[0] == 'setting' ]
-	urlpaths = [ urllib.parse.urlparse(value) for setting, value in settings if setting == 'au_manifest' ]
-	basenames = [ os.path.basename(url.path) for url in urlpaths ]
 	
+	urlpaths = [ urllib.parse.urlparse(value) for setting, value in settings if setting == 'au_manifest' ]
+	if len(urlpaths) == 0 :
+		urlpaths = [ urllib.parse.urlparse(value) for setting, value in settings if setting == 'au_start_url' ]
+
+	basenames = [ os.path.basename(url.path) for url in urlpaths ]
+
 	parameters = json.loads(switches['parameters'])
 	parameters = {**dict(settings), **parameters}
-	
+
 	for file in basenames :
-		infile = "%(path)s/%(file)s.sprintf" % {"path": scriptdir, "file": file}
-		outfile = "./%(file)s" % {"file": file}
+		infile = "%(path)s/templates/%(file)s.sprintf" % {"path": scriptdir, "file": file}
+		outdir = get_filesystemlocation(switches['local']) if switches['local'] is not None else "."
+		outfile = "%(path)s/%(file)s" % {"path": outdir, "file": file}
 		
 		instream = open(infile, "r")
 		intext = "\n".join([ line.rstrip() for line in instream ])
 		instream.close()
-		print(intext % parameters)
+		
+		if "-" == switches['local'] :
+			print(intext % parameters)
+		else :
+			hOut = open(outfile, "w")
+			outtext = (intext % parameters)
+			print(intext % parameters, file=hOut)
+			hOut.close()
+			print("%(filename)s\t%(size)d\n" % {"filename": outfile, "size": os.stat(outfile).st_size})
